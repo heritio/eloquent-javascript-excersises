4c9daaed10469b96188a05b4d75a4fa6f8820232
heritio <66847929+heritio@users.noreply.github.com>
2020-10-06 23:07:51 +0200

    Added a function that will abstract a counting in a program from eloquent javascript excersises

--- script.js
+++ script.js
@@ -2249,3 +2249,25 @@ function characterScript(code){
 
 //Calling characterScript like this characterScript(120) -> would return a script with that code in a range.
 
+//This function with deliver an array with an object with true or false after counting elements that are passed through, and deliver a obj like this
+//[{name:true, count:1},{name: false, count: 1}] indicating the elements that pass a certain test.
+
+function countBy(items, groupName){
+ let counts = [];
+  for(let item of items){
+   let name = groupName(item);
+    let known = counts.findIndex(c => c.name == name);
+    if(known = -1){
+      counts.push({name, count: 1}); 
+    }else{
+     counts[known].count++; 
+    }
+  }
+  return counts;
+}
+
+console.log(countBy([1, 2, 3, 4, 5], n => n > 2));
+// → [{name: false, count: 2}, {name: true, count: 3}]
+
+
+
